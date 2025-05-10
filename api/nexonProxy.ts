d71019853cbdbd19eae9ff3c6bbfafc97298d230
ruvice <andrewwee97@gmail.com>
2025-05-10 21:17:40 +0800

    adding config

--- api/nexonProxy.ts
+++ api/nexonProxy.ts
@@ -1,22 +1,26 @@
 export default async function handler(req: Request): Promise<Response> {
-    const { searchParams } = new URL(req.url);
+    const { searchParams } = new URL(req.url, `http://localhost`);
+
+
+    console.log('something')
   
     const path = searchParams.get("path");
-    const apiKey = process.env.OPEN_API_KEY;
+    const apiKey = process.env.OPEN_API_KEY2;
     const domain = 'https://open.api.nexon.com'
   
     if (!path || !apiKey || !domain) {
-      return new Response(JSON.stringify({ error: "Missing path or env vars" }), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
+        console.log('wtf', path, apiKey, domain)
+        return new Response(JSON.stringify({ error: "Missing path or env vars" }), {
+            status: 400,
+            headers: { "Content-Type": "application/json" },
+        });
     }
   
     const url = new URL(`${domain}/${path}`);
     for (const [key, value] of searchParams.entries()) {
       if (key !== "path") url.searchParams.append(key, value);
     }
-  
+    console.log(url)
     const response = await fetch(url.toString(), {
       headers: { "x-nxopen-api-key": apiKey },
     });
@@ -27,4 +31,7 @@ export default async function handler(req: Request): Promise<Response> {
       headers: { "Content-Type": "application/json" },
     });
 }
-  
+
+export const config = {
+    runtime: "edge",
+};
