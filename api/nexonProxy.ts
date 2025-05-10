39ed82b771e45f56515ffab6cab7e10052de188c
ruvice <andrewwee97@gmail.com>
2025-05-10 21:38:46 +0800

    adding CORS

--- api/nexonProxy.ts
+++ api/nexonProxy.ts
@@ -1,18 +1,34 @@
 export default async function handler(req: Request): Promise<Response> {
     const { searchParams } = new URL(req.url, `http://localhost`);
-
-
-    console.log('something')
   
     const path = searchParams.get("path");
     const apiKey = process.env.OPEN_API_KEY2;
     const domain = 'https://open.api.nexon.com'
+    const allowedOrigins = [
+        "https://localhost:8080",
+        "https://extension-files.twitch.tv",
+        "https://vgxcnnkl2o4t2k8fbdrqszhbphh9pc.ext-twitch.tv/"
+    ];
+      
+    const origin = req.headers.get("origin") || req.headers.get("referer");
+      
+    if (!origin || !allowedOrigins.some(o => origin.startsWith(o))) {
+        return new Response(JSON.stringify({ error: "Forbidden" }), {
+            status: 403,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "null" // block
+            }
+        });
+    }
   
     if (!path || !apiKey || !domain) {
-        console.log('wtf', path, apiKey, domain)
         return new Response(JSON.stringify({ error: "Missing path or env vars" }), {
             status: 400,
-            headers: { "Content-Type": "application/json" },
+            headers: { 
+                "Content-Type": "application/json", 
+                "Access-Control-Allow-Origin": origin 
+            },
         });
     }
   
@@ -27,8 +43,11 @@ export default async function handler(req: Request): Promise<Response> {
   
     const body = await response.text(); // Forward raw response
     return new Response(body, {
-      status: response.status,
-      headers: { "Content-Type": "application/json" },
+        status: response.status,
+        headers: { 
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": origin
+        },
     });
 }
 
